3bba17a988b5d9cc87a2b07ab865c14116007f82
tresoldi.lorenzo <tresoldi.lorenzo@TAVE-PC32.OSDBSESTO.local>
2021-04-15 14:09:32 +0200

    sium

--- src/serializzazione/Serializzazione.java
+++ src/serializzazione/Serializzazione.java
@@ -5,6 +5,15 @@
  */
 package serializzazione;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author tresoldi.lorenzo
@@ -14,8 +23,40 @@ public class Serializzazione {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException  {
+        Studente stud = new Studente("francesco", "sium", 467375);
+        
+        try {
+//            FileOutputStream FO = new FileOutputStream("test.ser");
+//            ObjectOutputStream OS = new ObjectOutputStream(FO);        
+//        OS.writeObject(9);
+//        OS.writeObject("ciao");
+//        OS.writeObject(0.444324);
+//        OS.writeObject('a');
+//        OS.flush();
+//        OS.close();
+//        FO.close();
+
+        FileInputStream FI = new FileInputStream("test.ser");
+        ObjectInputStream IS = new ObjectInputStream(FI);
+            int v1 = (int) IS.readObject();
+            System.out.println("V1:" + v1);
+            String v2 = (String) IS.readObject();
+            System.out.println("ciao  :" + v1);
+        
+        
+        } catch (FileNotFoundException ex) {
+            System.out.println("file non trovto");
+        }
+         catch (IOException ex) {
+             System.out.println("hai rotto il java");
+        
+        
     }
-    
+
+
+
+    }
 }
+    
+
